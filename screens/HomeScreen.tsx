a2e70a034dfd41d5354ff701ba38adf634b2c584
Yeroglif <n.skoblik@ukr.net>
2025-08-26 11:55:20 +0300

    fix: resolve homescreen scrolling and remove expense list

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
-import ExpenseList from "../components/Home/ExpenseList";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  FlatList,
+} from "react-native";
 import ExpenseSummary from "../components/Home/ExpenseSummary";
 import ExpenseFilter from "../components/Home/ExpenseFilter";
 import { useExpenses } from "../context/ExpenseContext";
 import ExpenseChart from "../components/Home/ExpenseChart";
+import ExpenseItem from "../components/Home/ExpenseItem";
 
 interface HomeScreenProps {
   navigation: any;
@@ -12,24 +18,35 @@ interface HomeScreenProps {
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const { setFilter } = useExpenses();
+  const { expenses, setFilter, deleteExpense } = useExpenses();
 
   return (
     <View style={styles.container}>
-      <View style={{ marginBottom: 75 }}>
-        <ExpenseSummary />
-      </View>
-      <ExpenseChart />
-      <View style={{ marginTop: 30 }}>
-        <ExpenseFilter
-          selectedCategory={selectedCategory}
-          onCategoryChange={(category) => {
-            setSelectedCategory(category);
-            setFilter(category);
-          }}
-        />
-      </View>
-      <ExpenseList />
+      <FlatList
+        data={expenses}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ExpenseItem expense={item} onDelete={deleteExpense} />
+        )}
+        ListHeaderComponent={
+          <View>
+            <View style={{ marginBottom: 30 }}>
+              <ExpenseSummary />
+            </View>
+            <ExpenseChart />
+            <View style={{ marginTop: 30 }}>
+              <ExpenseFilter
+                selectedCategory={selectedCategory}
+                onCategoryChange={(category) => {
+                  setSelectedCategory(category);
+                  setFilter(category);
+                }}
+              />
+            </View>
+          </View>
+        }
+        contentContainerStyle={{ paddingBottom: 120 }}
+      />
 
       <TouchableOpacity
         style={styles.addButton}
@@ -45,7 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    gap: 20,
   },
   addButton: {
     position: "absolute",
